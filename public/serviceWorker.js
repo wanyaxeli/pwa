a21b3d0c953161a246e7ff73f5b673ab16778fe9
wanyaxeli <eliwanyax@gmail.com>
2024-04-25 04:02:07 +0300

    added ws

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -17,3 +17,27 @@ self.addEventListener('activate',e=>{
      console.log('activated service worker ')
     e.waitUntil(self.clients.claim())
 })
+self.addEventListener('fetch',e=>{
+    console.log(`fetching ${e.request.url}`)
+    if (navigator.onLine){
+      let fetchRequest=e.request.clone()
+      return fetch(fetchRequest).then(res=>{
+        if (!res|| res.status !==200 || res.type ==='basic'){
+            return res
+        }
+        let resonseTocache=res.clone()
+        caches.open(cache_name).then(cache=>{
+            cache.put(e.request,resonseTocache)
+            return res
+        })
+    })
+    }
+    else{
+        e.respondWith(
+            caches.match(e.request)
+            .then(res=>{
+                return res
+            })
+        )
+    }
+})
